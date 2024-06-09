fa6f649e94ae6f511388f6d240ae49a41866d995
Carlos PC7 <formacionescpc@gmail.com>
2024-06-09 18:06:44 +0200

    rehaciendo

--- src/juego.ts
+++ src/juego.ts
@@ -7,11 +7,9 @@ const numeroAleatorio = generarNumeroAleatorio();
 const carta = generarCarta(numeroAleatorio);
 const urlCarta = obtenerUrlCarta (carta);
 mostrarImagen(urlCarta);
-
 sumarANuevaPuntuacion(carta);
 mostrarPuntuacion();
 gestionarPartida();
-
 apareceBoton();
 };
 
@@ -52,7 +50,11 @@ const obtenerUrlCarta = (carta: number) => {
 
 const mostrarImagen = (urlCarta: string) => {
     const elementoImagen = document.getElementById("imagenCarta");
-            if (elementoImagen !== null && elementoImagen !== undefined && elementoImagen instanceof HTMLImageElement) {
+            if (
+                elementoImagen !== null &&
+                elementoImagen !== undefined &&
+                elementoImagen instanceof HTMLImageElement
+            ) {
                 elementoImagen.src = urlCarta;
             } else {
                 console.error("Elemento img no encontrado en el DOM");
@@ -60,7 +62,6 @@ const mostrarImagen = (urlCarta: string) => {
 };
 
 let guardarPuntuacion: number = 0;
-
 
 const mostrarPuntuacion = () => {
     const elementoPuntuacion = document.getElementById("puntuacion");
@@ -70,56 +71,73 @@ const mostrarPuntuacion = () => {
 };
 
 const sumarANuevaPuntuacion = (carta: number) => {
-    if (carta > 7) {guardarPuntuacion +=0.5}
-    else {guardarPuntuacion += carta};
+    if (carta > 7) {
+        guardarPuntuacion +=0.5;
+    } else {
+        guardarPuntuacion += carta
+    }
 };
 
 const totalPuntuacion = 7.5;
 
-const gameOver = () => {
-    const elementoMensaje = document.getElementById("mensaje");
-    if(guardarPuntuacion > totalPuntuacion && elementoMensaje !== null && elementoMensaje !== undefined) {
-        elementoMensaje.innerHTML = `GAME OVER. HAS SUPERADO EL MÁXIMO DE PUNTUACIÓN!!`;
-    }
-};
-
-
 const deshabilitarBoton = () => {
     if (guardarPuntuacion > totalPuntuacion) {
         const elementoButton = document.getElementById("carta");
-        if (elementoButton!== null && elementoButton!== undefined && elementoButton && elementoButton instanceof HTMLButtonElement) {
+        if (
+            elementoButton!== null &&
+            elementoButton!== undefined &&
+            elementoButton &&
+            elementoButton instanceof HTMLButtonElement
+        ) {
             elementoButton.disabled = true;
         } else {
-            console.error ("gestionarGameOver: No se ha encontrado el elemendo id con Button")
+            console.error ("gestionarGameOver: No se ha encontrado el elemendo id con Button"
+            );
         }
     }
 };
 
 const gestionarPartida = () => {
-    if (guardarPuntuacion > totalPuntuacion) {
-        gameOver();
-        deshabilitarBoton();
+    if (guardarPuntuacion === 7.5) {
+      mostrarMensaje("He ganado");
     }
-};
+  
+    if (guardarPuntuacion > totalPuntuacion) {
+      mostrarMensaje("GAME OVER. HAS SUPERADO EL MÁXIMO DE PUNTUACIÓN!!");
+      deshabilitarBoton();
+    }
+  };
 
-if(botonEmpezarPartida !== null && botonEmpezarPartida !== undefined && botonEmpezarPartida instanceof HTMLButtonElement) {
+if(
+botonEmpezarPartida !== null &&
+botonEmpezarPartida !== undefined &&
+botonEmpezarPartida instanceof HTMLButtonElement
+) {
 botonEmpezarPartida.addEventListener("click",empezarPartida)
-}
+};
 
 const botonMePlanto = document.getElementById("meplanto");
-
+ 
 const seAcabaPartida = () => {
-nosPlantamos();
-puntuacionObtenida();
-apareceBoton();
-};
+    nosPlantamos();
+    const mensajeMeRindo = puntuacionObtenida();
+    mostrarMensaje(mensajeMeRindo);
+    apareceBoton();
+  };
 
 const nosPlantamos =() => {
     const elementoButton = document.getElementById("carta");
-    if (elementoButton!== null && elementoButton !== undefined && elementoButton && elementoButton instanceof HTMLButtonElement) {
+    if (
+        elementoButton!== null &&
+        elementoButton !== undefined &&
+        elementoButton &&
+        elementoButton instanceof HTMLButtonElement
+    ) {
         elementoButton.disabled = true;
     } else {
-        console.error ("gestionarGameOver: No se ha encontrado el elemendo id con Button")
+        console.error (
+        "gestionarGameOver: No se ha encontrado el elemendo id con Button"
+    );
     }
 };
 
@@ -128,31 +146,48 @@ const numeroCinco = 5;
 const numeroSeis = 6;
 const numeroSiete = 7;
 const numeroSieteYMedio = 7.5;
-//
-const puntuacionObtenida = () => {
-    const elementoMensaje2 = document.getElementById("mensaje");
-    if(elementoMensaje2 !== null && elementoMensaje2 !== undefined) {
-        if(guardarPuntuacion <= numeroCuatro) {
-        elementoMensaje2.innerHTML = `Has sido muy conservador`;
-        }
-        if(guardarPuntuacion === numeroCinco) {
-            elementoMensaje2.innerHTML = `Te ha entrado el canguelo eh?`;
-        }
-        if(guardarPuntuacion === numeroSeis) {
-            elementoMensaje2.innerHTML = `Casi casi...`;
-        }
-        if(guardarPuntuacion <= numeroSiete) {
-            elementoMensaje2.innerHTML = `Casi casi...`;
-        }
-        if(guardarPuntuacion === numeroSieteYMedio) {
-            elementoMensaje2.innerHTML = `¡Lo has clavado! ¡Enhorabuena!`;
-        }
+
+const puntuacionObtenida = (): string => {
+    if (guardarPuntuacion <= numeroCuatro) {
+      return `Has sido muy conservador`;
     }
-};
+    if (guardarPuntuacion === numeroCinco) {
+      return `Te ha entrado el canguelo eh?`;
+    }
+    if (guardarPuntuacion === numeroSeis) {
+      return `Casi casi...`;
+    }
+    if (guardarPuntuacion <= numeroSiete) {
+      return `Casi casi...`;
+    }
+    if (guardarPuntuacion === numeroSieteYMedio) {
+      return `¡Lo has clavado! ¡Enhorabuena!`;
+    }
+  
+    if (guardarPuntuacion > 7.5) {
+      return "Game over";
+    }
+  
+    return "No deberías de llegar aquí";
+  };
 
+  const mostrarMensaje = (mensaje: string) => {
+    const elementoMensaje2 = document.getElementById("mensaje");
+    if (
+      elementoMensaje2 !== null &&
+      elementoMensaje2 !== undefined &&
+      elementoMensaje2 instanceof HTMLDivElement
+    ) {
+      elementoMensaje2.innerText = mensaje;
+    }
+  };
 
-if(botonMePlanto !== null && botonMePlanto !== undefined && botonMePlanto instanceof HTMLButtonElement) {
-    botonMePlanto.addEventListener("click",seAcabaPartida)
+if(
+    botonMePlanto !== null &&
+    botonMePlanto !== undefined &&
+    botonMePlanto instanceof HTMLButtonElement
+    ) {
+    botonMePlanto.addEventListener("click",seAcabaPartida);
     };
 
 
@@ -163,52 +198,44 @@ if(botonMePlanto !== null && botonMePlanto !== undefined && botonMePlanto instan
         resetPartida();
     };
 
-    const apareceBoton =() => {
-        const elementoButton2 = document.getElementById("carta");
+    const apareceBoton = () => {
         const elementoButton3 = document.getElementById("reiniciar");
-        if (elementoButton2!== null && elementoButton2!== undefined
-            && elementoButton2 instanceof HTMLButtonElement && elementoButton2.disabled
-            && elementoButton3!== null && elementoButton3!== undefined && elementoButton3 instanceof HTMLButtonElement) {
-            elementoButton3.disabled = false;
+        if (
+          elementoButton3 !== null &&
+          elementoButton3 !== undefined &&
+          elementoButton3 instanceof HTMLButtonElement
+        ) {
+          elementoButton3.disabled = false;
         } else {
-            console.error ("Error not found 404")
+          console.error("Error not found 404");
         }
-    };
+      };
 
     const vamosAPorOtraPartida = () => {
         const elementoButton2 = document.getElementById("carta");
-        const elementoButton3 = document.getElementById("reiniciar");
-        if (elementoButton2!== null && elementoButton2!== undefined && elementoButton2 instanceof HTMLButtonElement
-            && elementoButton2.disabled && elementoButton3!== null && elementoButton3!== undefined
-            && elementoButton3 instanceof HTMLButtonElement && elementoButton3.disabled === false) {
-            elementoButton2.disabled = false;
+        if (
+          elementoButton2 !== null &&
+          elementoButton2 !== undefined &&
+          elementoButton2 instanceof HTMLButtonElement
+        ) {
+          elementoButton2.disabled = false;
         } else {
-            console.error ("Error not found 404")
+          console.error("Error not found 404");
         }
-    };
+      };
 
     const resetPartida = () => {
-        const numeroPartida = document.getElementById("puntuacion");
-        if(numeroPartida !== null && numeroPartida !== undefined && numeroPartida instanceof HTMLDivElement) {
-            numeroPartida.innerHTML = ' ';
-        }
-        const imagenCarta = document.getElementById("imagenCarta");
-            if (imagenCarta !== null && imagenCarta !== undefined && imagenCarta instanceof HTMLImageElement) {
-                imagenCarta.src = ' ';
-            } else {
-                console.error("Elemento img no encontrado en el DOM");
-            }
-        const mensajePartida = document.getElementById("mensaje");
-        if(mensajePartida !== null && mensajePartida !== undefined && mensajePartida instanceof HTMLDivElement){
-            mensajePartida.innerHTML = ' ';
-        }
-        const iniciarPuntuacion = document.getElementById("puntuacion");
-        if(iniciarPuntuacion !== null && iniciarPuntuacion !== undefined && iniciarPuntuacion instanceof HTMLDivElement) {
-            guardarPuntuacion = 0;
-        }
-    };
+        guardarPuntuacion = 0;
+        mostrarPuntuacion();
+        mostrarImagen("");
+        mostrarMensaje("");
+      };
 
 
-    if(botonReiniciar !== null && botonReiniciar !== undefined && botonReiniciar instanceof HTMLButtonElement) {
-        botonReiniciar.addEventListener("click",seReiniciaPartida)
-        };
+    if (
+        botonReiniciar !== null &&
+        botonReiniciar !== undefined &&
+        botonReiniciar instanceof HTMLButtonElement
+      ) {
+        botonReiniciar.addEventListener("click", seReiniciaPartida);
+      };
